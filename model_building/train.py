46c87035c8c4af5acebf73018d80720f275de1b2
Sandeep Bhutani <sandtemp01@gmail.com>
2026-02-21 15:44:08 +0530

    Update train.py

--- model_building/train.py
+++ model_building/train.py
@@ -127,7 +127,7 @@ with mlflow.start_run():
     print(f"Model saved as artifact at: {model_path}")
 
     # Upload to Hugging Face
-    repo_id = "praneeth232/machine_failure_model"
+    repo_id = "sandtemp01/machine_failure_model"
     repo_type = "model"
 
     # Step 1: Check if the space exists
